f5d2d9473c85e204413841c7ce61be034f3d2353
ganeshjatms@gmail.com <ganeshjatms@gmail.com>
2023-06-23 18:49:12 +0530

    jenkins first pipeline

---
+++ test.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent { label "dev" }
+    stages {
+        stage('cleanup') {
+            steps {
+                sh 'cd $WORKSPACE'
+                sh 'docker stop eager_mahavira || true'
+                sh 'docker rm -f eager_mahavira || true'
+                // sh 'docker-compose down || true'
+            }
+        }
+        stage('build') {
+            steps {
+                  sh 'cd $WORKSPACE'
+                  sh 'docker pull pengbai/docker-supermario:latest'
+                  sleep(time: 3, unit: "SECONDS")
+                }
+        }
+        stage('deploy') {
+            steps {
+                sh 'cd $WORKSPACE && docker run -d -p 8600:8080 pengbai/docker-supermario'
+                sleep(time: 3, unit: "SECONDS")
+            }
+        }
+    }
